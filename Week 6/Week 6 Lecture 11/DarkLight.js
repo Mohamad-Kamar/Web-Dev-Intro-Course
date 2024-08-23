872d26a0a54a1f513db8db03c092302a8791f05e
mkamar <mohamad.kamar@agoda.com>
2024-08-23 07:43:44 +0300

    Add light and dark mode exercise solution

---
+++ Week 6/Week 6 Lecture 11/DarkLight.js
@@ -0,0 +1,10 @@
+let body = document.querySelector('body');
+function turnOnDark(){
+    body.style.backgroundColor = 'black';
+    body.style.color = 'white';    
+}
+
+function turnOnLight(){
+    body.style.backgroundColor = 'white';
+    body.style.color = 'black';    
+}
